c772e0b5ad0b30496918395a08f39f663efb7365
Julyanne Correia <julyanne.es@gmail.com>
2023-03-17 16:01:46 -0300

    feat: routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={ <Home/> } path='/'/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
