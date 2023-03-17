005ce8ef36cc07811d1c7cf352517484b44acce5
Julyanne Correia <julyanne.es@gmail.com>
2023-03-17 16:01:18 -0300

    feat: header component

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+import smartTrash from './../../assets/smartTrash.png';
+import './header.css';
+
+function Header(){
+
+    return(
+        <div className='container'>
+            <div>
+                <img src={smartTrash} alt='logo'/>
+            </div>
+
+            <Link to='/'>
+                Home
+            </Link>
+            <Link to='/contact'>
+                Contato
+            </Link>
+            <Link to='/signup'>
+                Cadastro
+            </Link>
+            <button><Link to='/signin'>
+                <span>Login</span>
+            </Link></button>
+        </div>
+    );
+}
+
+export default Header;
