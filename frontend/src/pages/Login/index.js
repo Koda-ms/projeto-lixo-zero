f672ac9f81972ad649944e3eac7bc64fa4d1fba8
Julyanne Correia <julyanne.es@gmail.com>
2023-03-23 10:28:26 -0300

    style: login page changing

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
+import formLogo from '../../assets/undraw_login_re_4vu2.svg';
 import './login.css';
 
 function Login(){
@@ -8,17 +9,30 @@ function Login(){
         <div>
             <Header/>
         
-            <div className='login-container'>
-                <div className='login-title'>
-                    <h2>Criar conta</h2>
-                </div>
-                <div className='logo'>
-                    <form className='form'>
-                        <input type='text' placeholder='Email'/>
-                        <input type='password' placeholder='Password' />
-                        <button>Sign In</button>
-                    </form>
-                    <Link to='/signup'>Don't have an account? Sign up here!</Link>
+            <div className='signup-container'>
+                <div className='signup-content'>
+                    <div className='signup-image'>
+                        <img src={formLogo} alt='form-logo'/>
+                    </div>
+                    <div className='form-box'>
+                        <form className='form-content'>
+                            <div className='form-header'>
+                                <h2>Acessar conta</h2>
+                            </div>
+                            <div className='input-group'>
+                                <div className='input-box-login'>
+                                    <input type='text' placeholder='Email' required/>
+                                </div>
+                                <div className='input-box-login'>
+                                    <input type='password' placeholder='Senha' required/>
+                                </div>
+                            </div>
+                            <div className='signup-btn'>
+                                <button>Entrar</button>
+                            </div>
+                        </form>
+                        <Link to='/signup'>Não possui uma conta? Crie uma aqui!</Link>
+                    </div>
                 </div>
             </div>
         </div>
