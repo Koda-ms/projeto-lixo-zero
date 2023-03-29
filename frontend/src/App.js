e48a0eceb2eaf2eb5d7a41aed606bc009fcfb788
Julyanne Correia <julyanne.es@gmail.com>
2023-03-29 15:55:47 -0300

    feat: profile page and route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route element={ <Home/> } path='/'/>
         <Route element={ <Login/> } path='/login'/>
         <Route element={ <Signup/> } path='/signup'/>
+        <Route element={ <Profile/> } path='/profile'/>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,14 @@
+import Sidebar from '../../components/Sidebar';
+import './profile.css';
+
+function Profile() {
+
+    
+    return(
+        <div>
+            <Sidebar/>
+        </div>
+    );
+}
+
+export default Profile;
