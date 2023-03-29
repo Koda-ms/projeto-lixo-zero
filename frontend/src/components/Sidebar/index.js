ec049e63a7efc544ec8882be03218f6b5f6f32d8
Julyanne Correia <julyanne.es@gmail.com>
2023-03-29 15:58:16 -0300

    feat: sidebar component

---
+++ frontend/src/components/Sidebar/index.js
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import { TfiMapAlt, TfiPanel } from 'react-icons/tfi';
+import { SlLogout } from 'react-icons/sl';
+import profileAvatar from '../../assets/undraw_profile_pic_re_iwgo.svg';
+import './sidebar.css';
+import '../../config';
+
+function Sidebar() {
+    return(
+        <div className='sidebar'>
+            <div>
+                <img src={profileAvatar} alt='#'/>
+            </div>
+
+            <Link to='/panel'>
+                <TfiPanel color='#fff' size={24}/>
+                Painel de sensores
+            </Link>
+            <Link to='/routes'>
+                <TfiMapAlt color='#fff' size={24}/>
+                Rota de coleta
+            </Link>
+            <Link to='/logout'>
+                <SlLogout color='#fff' size={24}/>
+                Sair
+            </Link>
+            
+        </div>
+    );
+}
+
+export default Sidebar;
