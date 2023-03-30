26c6c054d11706a9716c99c90cda5373d1e7ec32
Julyanne Correia <julyanne.es@gmail.com>
2023-03-29 23:51:45 -0300

    feat: title component

---
+++ frontend/src/components/Title/index.js
@@ -0,0 +1,12 @@
+import './title.css';
+
+function Title() {
+    
+    return(
+        <div className='title'>
+            <span>Painel de Sensores</span>
+        </div>
+    );
+}
+
+export default Title;
