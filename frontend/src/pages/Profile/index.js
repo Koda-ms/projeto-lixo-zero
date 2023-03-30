891b7f7accdc186a4cb78059a18e338fee535d22
Julyanne Correia <julyanne.es@gmail.com>
2023-03-29 23:52:28 -0300

    feat: profile (still working on it)

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,12 +1,18 @@
 import Sidebar from '../../components/Sidebar';
+import Title from '../../components/Title';
 import './profile.css';
 
 function Profile() {
 
-    
     return(
         <div>
             <Sidebar/>
+            <div className='profile-container'>
+                <Title/>
+                <div className='profile-content'>
+
+                </div>
+            </div>
         </div>
     );
 }
