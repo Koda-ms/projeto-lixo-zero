79f08cd9f5c36ec0b332a290db3b11f44b811524
Julyanne Correia <julyanne.es@gmail.com>
2023-04-02 11:44:49 -0300

    feat: requisição de dados do sensor

--- frontend/src/pages/Panel/index.js
+++ frontend/src/pages/Panel/index.js
@@ -11,41 +11,42 @@ function Panel() {
     useEffect(() => {
 
         async function getStatus() {
-            let response = await axios.get('/status/lz-get-status')
+            let response = await axios.get('/status/lz-get-status', { 
+                headers: {
+                    'Ocp-Apim-Subscription-Key': 'fb3b51c48ae843aca4d099c759a732f9'
+                }
+            })
             .then((response) => response.data)
             .catch((error) => {
                 console.log(error);
             })
-            console.log(response);
-            setStatus(response);
+            // console.log(response);
+            setStatus(response.status_trash); 
         }
         getStatus();
     }, []);
-    console.log(status.status_trash.full);
+    // console.log(status.status_trash.full);
 
     return(
         <div>
             <Sidebar/>
             <div className='panel-content'>
                 <Title/>
-                    {/* {status.map((item) => {
-                        return( */}
-                            <div className='panel-container'>
+                    <div className='panel-container'>
 
-                                <div style={{'background':'linear-gradient(45deg, #FF5370, #ff869a)'}} className='box-info'>
-                                    <h3>Lixeiras Cheias</h3>
-                                    <span>{status.status_trash.full}</span>
-                                </div>
-                                <div style={{'background':'linear-gradient(45deg, #4099ff, #73b4ff)'}} className='box-info'>
-                                    <h3>Lixeiras Medianas</h3>
-                                    {/* <span>{status.status_trash.medium}</span> */}
-                                </div>
-                                <div style={{'background':'linear-gradient(45deg, #38e544, #82f789)'}} className='box-info'>
-                                    <h3>Lixeiras Vazias</h3>
-                                    {/* <span>{status.status_trash.empty}</span> */}
-                                </div>
-                            </div>
-                    {/* )})}*/}
+                        <div style={{'background':'linear-gradient(45deg, #FF5370, #ff869a)'}} className='box-info'>
+                            <h3>Lixeiras Cheias</h3>
+                            <span>{status.full}</span>
+                        </div>
+                        <div style={{'background':'linear-gradient(45deg, #4099ff, #73b4ff)'}} className='box-info'>
+                            <h3>Lixeiras Medianas</h3>
+                            <span>{status.medium}</span>
+                        </div>
+                        <div style={{'background':'linear-gradient(45deg, #17ae21, #1bcd27)'}} className='box-info'>
+                            <h3>Lixeiras Vazias</h3>
+                            <span>{status.empty}</span>
+                        </div>
+                    </div>
             </div>
         </div>
     );
