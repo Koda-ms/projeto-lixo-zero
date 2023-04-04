24ab5004e41c0aa274dc2d2e46b7a1d08e46877c
marissolguerra <marissolguerra.sc@gmail.com>
2023-04-03 22:34:18 -0300

    feature: contact

--- frontend/src/pages/Contact/index.js
+++ frontend/src/pages/Contact/index.js
@@ -8,34 +8,34 @@ function Contact(){
         <div>
             <Header/>
 
-            <div className='contact-container'>
-                <div className='contact-content'>
-                    <div className='contact-image'>
-                        <img src={formLogoContact} alt='form-logo-contact'/>
+            <div className='signup-container'>
+                <div className='signup-content'>
+                    <div className='signup-image'>
+                        <img src={formLogoContact} alt='form-logo'/>
                     </div>
-                    <div className='contact-form-box'>
-                        <form className='contact-form-content'>
-                            <div className='contact-form-header'>
+                    <div className='form-box'>
+                        <form className='form-content'>
+                            <div className='form-header'>
                                 <h2>Envie uma mensagem</h2>
                             </div>
                             <div className='input-group'>
-                                <div className='input-box-contact'>
+                                <div className='input-box'>
                                     <input type='text' placeholder='Seu nome' required/>
                                 </div>
-                                <div className='input-box-contact'>
+                                <div className='input-box'>
                                     <input type='text' placeholder = 'Empresa'/>
                                 </div>
-                                <div className='input-box-contact'>
+                                <div className='input-box'>
                                     <input type='text' placeholder='E-mail' required/>
                                 </div>
-                                <div className='input-box-contact'>
+                                <div className='input-box'>
                                     <input type='text' placeholder='Assunto' required/>
                                 </div>
-                                <div className='textarea-box-contact'>
+                                <div className='input-msg-box'>
                                     <textarea type='text' placeholder='Escreva sua mensagem' required/>
                                 </div>
                             </div>
-                            <div className='contact-btn'>
+                            <div className='signup-btn'>
                                 <button>Enviar</button>
                             </div>
                         </form>
